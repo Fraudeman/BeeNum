e52d90c97202a444432595054834075486412822
Fraudeman <49951672+Fraudeman@users.noreply.github.com>
2024-09-18 02:15:56 +0200

    Add files via upload
    Initial Commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "PanPam-My project-1.0.2";
+const contentToCache = [
+    "Build/Bee Number 800 x 600.loader.js",
+    "Build/Bee Number 800 x 600.framework.js.unityweb",
+    "Build/Bee Number 800 x 600.data.unityweb",
+    "Build/Bee Number 800 x 600.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
